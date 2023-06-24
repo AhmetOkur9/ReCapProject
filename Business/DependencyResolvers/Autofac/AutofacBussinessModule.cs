54d2a92199f4fb834e40228acbc9773071db7485
AhmetOkur9 <okur.ahmetkaangg@gmail.com>
2023-06-24 14:21:03 +0300

    24,06,2023

--- Business/DependencyResolvers/Autofac/AutofacBussinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBussinessModule.cs
@@ -3,6 +3,7 @@ using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
 using Castle.DynamicProxy;
+using Core.Utilities.Helpers.FileHelp;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using System;
@@ -22,7 +23,12 @@ namespace Business.DependencyResolvers.Autofac
 
             builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
             builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
-            
+
+            builder.RegisterType<CarImageManager>().As<ICarImageService>().SingleInstance();
+            builder.RegisterType<EfCarImageDal>().As<ICarImageDal>().SingleInstance();
+
+            builder.RegisterType<FileHelperManager>().As<IFileHelper>().SingleInstance();
+
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
             builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
